bfdf02191125d2714839e0b93630195597ae39b4
caramylle <camille.manlapaz22@gmail.com>
2025-12-14 17:24:48 +0800

    feat: landing page

---
+++ vue-app/src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="text-white min-h-screen flex bg-repeat items-center" style="background-image: url('https://picsum.photos/1800/800?random=1');"> 
+
+    <div class="container mx-auto px-6 lg:px-20">
+        <div class="max-w-2xl">
+            <h1 class="text-2xl font-extrabold mb-6">
+                Build Your Own Projects</h1>
+
+                <p class="text-lg mb-8 text-white">
+                    Explore the World of IT. Build on your own! </p>
+
+                    <div class="flex flex-wrap gap-4">
+                        <a href="#signup" 
+                        class="bg-blue-500 text-white-600 font-semibold px-6 py-3 rounded-lg shadow hover:bg-gray-100 hover:text-black transition"> Get Started</a>
+
+                        <a href="#" 
+                        class="border border-white text-white font-semibold px-6 py-3 rounded-lg shadow hover:bg-gray-100 hover:text-black transition"> Explore Courses</a>
+                    </div>
+        </div>
+        </div>
+    </div>
+</template>
---
+++ vue-app/src/components/Skills.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="min-h-screen flex justify-center items-center bg-rose-200"> 
+        SKILLS SECTION
+    </div>
+</template>
